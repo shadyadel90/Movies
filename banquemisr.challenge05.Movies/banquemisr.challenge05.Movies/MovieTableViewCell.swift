3e381ed1dd18596521010f7015a4012b3e7d0114
Shady Adel <shadyadel910@gmail.com>
2024-09-27 21:06:58 +0300

    feat: Project intialed + basic design to all screens

--- banquemisr.challenge05.Movies/banquemisr.challenge05.Movies/MovieTableViewCell.swift
+++ banquemisr.challenge05.Movies/banquemisr.challenge05.Movies/MovieTableViewCell.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class MovieTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    
+    @IBOutlet weak var lblReleaseDate: UILabel!
+   
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
